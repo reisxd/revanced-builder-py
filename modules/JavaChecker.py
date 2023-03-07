3cd28bb5b169ca0a1c29f99ac8f29ae4a84ba031
reis <reis@turkeylist.gq>
2023-03-07 05:39:50 +0000

    fix: fix java check for java distributions

--- modules/JavaChecker.py
+++ modules/JavaChecker.py
@@ -1,15 +1,25 @@
 import subprocess
 import re
 
+# https://stackoverflow.com/a/19859308
+
+def hasNumbers(inputString):
+    return any(char.isdigit() for char in inputString)
+
 def CheckJDKInstalled():
     try:
-        result = subprocess.run(['java', '--version'], capture_output=True, text=True)
+        result = subprocess.run(['java', '-version'], capture_output=True, text=True)
         javaLog = result.stdout or result.stderr
         buildString = re.findall('\(.+?\)', javaLog)
-        if buildString == None:
+        if buildString == []:
             print('JDK is not installed.')
             return False
-        version = re.sub(r'[()]', '', buildString[0].replace('build ', ''))
+        indx = 0
+        for i in range(0, len(buildString)):
+            if hasNumbers(buildString[i]):
+                indx = i
+                break
+        version = re.sub(r'[()]', '', buildString[indx].replace('build ', ''))
         versionNumbers = version.split('.')
         if int(versionNumbers[0]) < 17 or 'openjdk' not in javaLog:
             print("JDK/Java was installed, but it's too old or not a JDK distribution.\nPlease install JDK from here: https://www.azul.com/downloads-new/?package=jdk")
